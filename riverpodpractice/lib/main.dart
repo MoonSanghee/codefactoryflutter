149cebcdf9305b3b62fad850e3e5289c9a22c755
문상희 <moonsanghee@munsanghuiui-MacBookPro.local>
2023-07-04 19:40:41 +0900

    ProviderObserver 실습

--- riverpodpractice/lib/main.dart
+++ riverpodpractice/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpodpractice/riverpod/provider_observer.dart';
 import 'package:riverpodpractice/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MaterialApp(
+    ProviderScope(
+      observers: [
+        Logger(),
+      ],
+      child: const MaterialApp(
         home: HomeScreen(),
       ),
     ),
