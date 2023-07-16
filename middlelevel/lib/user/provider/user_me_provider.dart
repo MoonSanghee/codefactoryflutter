2f3256ec2268ab65f8afedfe40fc948436788170
MoonSanghee <psycho13th@gmail.com>
2023-07-16 23:31:04 +0900

    Login과과 Token Refresh Repository, UserMeProvider 마무리하기

--- middlelevel/lib/user/provider/user_me_provider.dart
+++ middlelevel/lib/user/provider/user_me_provider.dart
@@ -2,13 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:middlelevel/common/const/data.dart';
 import 'package:middlelevel/user/model/user_model.dart';
+import 'package:middlelevel/user/repository/auth_repository.dart';
 import 'package:middlelevel/user/repository/user_me_repository.dart';
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final AuthRepository authRepository;
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
 
   UserMeStateNotifier({
+    required this.authRepository,
     required this.repository,
     required this.storage,
   }) : super(UserModelLoading()) {
@@ -26,5 +29,48 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     }
 
     final resp = await repository.getMe();
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      state = UserModelLoading();
+
+      final resp = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: REFRESH_TOKEN_KEY, value: resp.refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: resp.accessToken);
+
+      final userResp = await repository.getMe();
+
+      state = userResp;
+
+      return userResp;
+    } catch (e) {
+      state = UserModelError(message: '로그인에 실패하였습니다.');
+
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+
+    // 두 값을 한번에 삭제
+    await Future.wait(
+      [
+        storage.delete(key: REFRESH_TOKEN_KEY),
+        storage.delete(key: ACCESS_TOKEN_KEY),
+      ],
+    );
+
+    // 두 값을 차례대로 삭제
+    // await storage.delete(key: REFRESH_TOKEN_KEY);
+    // await storage.delete(key: ACCESS_TOKEN_KEY);
   }
 }
